3d5c6d7d0d663c0c1cd79c3eeffcea7488c3b729
maukai <mauricio.kazuto@gmail.com>
2021-06-29 13:56:21 -0300

    Segunda atualizacao

--- src/application/AulaTeste.java
+++ src/application/AulaTeste.java
@@ -1,10 +1,23 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class AulaTeste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um numero");
+		int x =  sc.nextInt();
+		System.out.println("O numero é: "+x);
+				
+		
+		
+				
+	
+		sc.close();
 	}
 
 }
